24079c786126526e3ed30184999d01172b0f425f
Bramus <bramus@bram.us>
2015-01-05 21:25:02 +0100

    Rewrite tests to work with bramus/ansi-php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,10 @@
 <?php
 
-	// Enable Error Reporting
-	error_reporting(E_ALL);
-	ini_set('display_errors', 'on');
+    // Enable Error Reporting
+    error_reporting(E_ALL);
+    ini_set('display_errors', 'on');
 
-	// Requires
-	require_once __DIR__ . '/../vendor/autoload.php';
+    // Requires
+    require_once __DIR__.'/../vendor/autoload.php';
 
 // EOF

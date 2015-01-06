33e8125337ebd37a086aaa48be1801e810f3242c
Bramus <bramus@bram.us>
2015-01-07 00:05:16 +0100

    More Unit Tests

--- tests/ColoredLineFormatterTest.php
+++ tests/ColoredLineFormatterTest.php
@@ -2,11 +2,14 @@
 
 use \Monolog\Logger;
 use \Bramus\Monolog\Formatter\ColoredLineFormatter;
+use Bramus\Ansi\Ansi;
+use Bramus\Ansi\ControlSequences\EscapeSequences\Enums\SGR;
 
 class ColoredLineFormatterTest extends PHPUnit_Framework_TestCase
 {
     protected function setUp()
     {
+        $this->ansi = new Ansi();
         $this->clf = new ColoredLineFormatter();
     }
 
@@ -23,6 +26,68 @@ class ColoredLineFormatterTest extends PHPUnit_Framework_TestCase
     public function testReset()
     {
         $this->assertEquals($this->clf->getColorScheme()->getResetString(), "\033[0m");
+    }
+
+    public function testDefaultColorScheme()
+    {
+        $defaultScheme = new \Bramus\Monolog\Formatter\ColorSchemes\DefaultScheme();
+
+        $this->assertEquals(
+            $this->clf->getColorScheme()->getColorizeArray(),
+            $defaultScheme->getColorizeArray()
+        );
+    }
+
+    public function testSetColorSchemeViaConstructor()
+    {
+        $newScheme = new \Bramus\Monolog\Formatter\ColorSchemes\TrafficLight();
+        $this->clf = new ColoredLineFormatter($newScheme);
+
+        $this->assertEquals(
+            $this->clf->getColorScheme()->getColorizeArray(),
+            $newScheme->getColorizeArray()
+        );
+    }
+
+    public function testSetColorSchemeViaSetColorScheme()
+    {
+        $newScheme = new \Bramus\Monolog\Formatter\ColorSchemes\TrafficLight();
+        $this->clf->setColorScheme($newScheme);
+
+        $this->assertEquals(
+            $this->clf->getColorScheme()->getColorizeArray(),
+            $newScheme->getColorizeArray()
+        );
+    }
+
+    public function testSetColorSchemeFilter()
+    {
+
+        $dummyArray = array(
+            Logger::DEBUG => $this->ansi->sgr(array(SGR::COLOR_FG_GREEN, SGR::STYLE_INTENSITY_FAINT))->get(),
+            '123' => 'foo',
+            9000 => 'bar',
+            Logger::INFO => $this->ansi->sgr(array(SGR::COLOR_FG_GREEN, SGR::STYLE_INTENSITY_NORMAL))->get(),
+            Logger::NOTICE => $this->ansi->sgr(array(SGR::COLOR_FG_GREEN, SGR::STYLE_INTENSITY_BRIGHT))->get(),
+            'foo' => 200,
+            Logger::WARNING => $this->ansi->sgr(array(SGR::COLOR_FG_YELLOW, SGR::STYLE_INTENSITY_FAINT))->get(),
+            Logger::ERROR => $this->ansi->sgr(array(SGR::COLOR_FG_YELLOW, SGR::STYLE_INTENSITY_NORMAL))->get(),
+            Logger::CRITICAL => $this->ansi->sgr(array(SGR::COLOR_FG_RED, SGR::STYLE_INTENSITY_NORMAL))->get(),
+            Logger::ALERT => $this->ansi->sgr(array(SGR::COLOR_FG_RED_BRIGHT, SGR::STYLE_INTENSITY_BRIGHT))->get(),
+            Logger::EMERGENCY => $this->ansi->sgr(array(SGR::COLOR_FG_RED_BRIGHT, SGR::STYLE_INTENSITY_BRIGHT, SGR::STYLE_BLINK))->get(),
+        );
+
+        $this->clf->getColorScheme()->setColorizeArray($dummyArray);
+
+        foreach(Logger::getLevels() as $level)
+        {
+            $this->assertArrayHasKey($level, $this->clf->getColorScheme()->getColorizeArray());
+        }
+
+        $this->assertArrayNotHasKey('123', $this->clf->getColorScheme()->getColorizeArray());
+        $this->assertArrayNotHasKey('foo', $this->clf->getColorScheme()->getColorizeArray());
+        $this->assertArrayNotHasKey(9000, $this->clf->getColorScheme()->getColorizeArray());
+
     }
 
     // public function testDemo()
